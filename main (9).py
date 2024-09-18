f7d77858e57e8986985176b11bec4d60602d06ca
David00310125 <daviddincan405@gmail.com>
2024-09-18 10:45:28 +0500

    Принцип ООП Полиморфизм

---
+++ main (9).py
@@ -0,0 +1,31 @@
+#Принцип ООП Полиморфизм (поли - много, морфизм - образ) - одноимённые методы в разных классах (метод с одним именем может выполнять разные функции)
+class Car:
+    def move(self):
+        print("Едет по дороге")
+
+class Moto:
+    def move(self):
+        print("Едет по дороге")
+
+class Train:
+    def move(self):
+        print("Едет по жд")
+
+class Boat:
+    def move(self):
+        print("Идёт по воде")
+
+lada = Car()
+voskhod = Moto()
+lastochka = Train()
+salut = Boat()
+
+print(lada.move())
+print(voskhod.move())
+print(lastochka.move())
+print(salut.move())
+            
+            
+            
+        
+            
